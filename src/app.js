9e5426e9f5581eae124c47528d66396241dde60f
Alfin123-stack <alfinkarin789@gmail.com>
2025-03-15 23:10:22 +0700

    Deploy Hapi.js ke Vercel

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-require('dotenv').config();
-const createServer = require('./Infrastructures/http/createServer');
-const container = require('./Infrastructures/container');
+require("dotenv").config();
+const createServer = require("./Infrastructures/http/createServer");
+const container = require("./Infrastructures/container");
 
-(async () => {
+module.exports = async (req, res) => {
   const server = await createServer(container);
-  await server.start();
-  console.log(`server start at ${server.info.uri}`);
-})();
+  await server.initialize(); // Ganti start() dengan initialize()
+
+  // Gunakan adapter Hapi untuk Vercel
+  await server
+    .inject({
+      method: req.method,
+      url: req.url,
+      payload: req.body,
+      headers: req.headers,
+    })
+    .then((response) => {
+      res.status(response.statusCode).send(response.result);
+    });
+};
